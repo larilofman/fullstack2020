fda42420c72fdeb87290b34ba10ceafa715b0554
Lari Löfman <larwazor@gmail.com>
2020-03-19 22:43:21 +0200

    add 7.7 country hook

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -22,7 +22,7 @@ const Anecdote = ({ anecdote }) => {
         <div>
             <h2>{anecdote.content} by {anecdote.author}</h2>
             <p>has {anecdote.votes} votes</p>
-            <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+            <p>for more info see <a href={anecdote.info} target="_blank">{anecdote.info}</a></p>
         </div>
     )
 }
